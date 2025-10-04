ec8665003dbe09efbf095c5d497d6963bbc10b7c
AlexSilva91 <alexsilvaalves17@outlook.com>
2025-10-04 00:00:49 -0300

    update

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 
 def create_app():
-    app = Flask(__name__)
+    app = Flask(__name__, static_folder="app/static", static_url_path="/static")
     app.config.from_object('app.config.Config')
 
     # Importa e registra as rotas
